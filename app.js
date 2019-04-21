3f58409fee9dbb9bfb64c06943cab0ee77e1345e
James Hatfield <kellywilbur@Kellys-MBP-2.attlocal.net>
2019-04-21 09:48:51 -0700

    test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connection.on('connected',  () => {
 
 app.use('/', authRoutes);
 app.get('*', (req,res)=>{
-    res.sendfile(path.join(__dirname + 'public/index.html'));
+    res.sendfile(path.join(__dirname, 'public/index.html'));
 });
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
