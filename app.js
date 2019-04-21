141dabcd9fa6d16da6f726e4b088ecaf2a0f10dc
James Hatfield <kellywilbur@Kellys-MBP-2.attlocal.net>
2019-04-21 10:56:11 -0700

    Changed db location

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const db = require('config/database');
+const db = require('./config/database');
 
 //Database Setup
 const mongoose = require('mongoose');
